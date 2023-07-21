b8cb4364f6f5e3f7277240c66134b1eb32439e51
ktorvi <me@ktorvi.me>
2023-07-21 17:02:33 +0200

    original V1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,147 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillLinkedin, AiFillInstagram, AiFillMail } from "react-icons/ai"
+import { Inter } from "next/font/google";
+import standing from "../../public/standing.png";
+import avatar from "../../public/avatar.png";
+import becode from "../../public/becode.png";
+import cisco from "../../public/cisco-ccna.png";
+import cs50 from "../../public/cs50.png";
+import pg from "../../public/postgresql.png";
+import python from "../../public/Python.png";
+import gc from "../../public/gc.png";
+import { useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className={darkMode ? "dark" : ""}>
+      <Head>
+        <title>Victor Philippe - ktorvi</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="bg-gray-300 dark:bg-gray-800 min-h-screen px-10 py-10 md:px-20 lg:px-40">
+        <section className="">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="dark:text-white text-2xl font-mono">ktorvi.me</h1>
+            <ul className="flex justify-center">
+              <li>
+                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="dark:text-white cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a className="bg-gradient-to-r from-teal-400 to-teal-600 text-white px-4 py-2 rounded-md ml-8" href="../../CV-VictorPhilippe-FR+EN.pdf" alt="alt text"
+                  target="_blank"
+                  rel="noopener noreferrer">Resume</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10 ">
+            <h2 className="text-5xl py-2 text-teal-700 md:text-6xl">Victor Philippe</h2>
+            <h3 className="dark:text-white  text-2xl md:text-3xl leading-8  py-5 text-gray-800 font-mono">Junior Developer</h3>
+            <p className="dark:text-white  text-md md:text-xl py-5 text-gray-800 max-width-lg">Junior developer seeking opportunities to grow and integrate a team, mainly self taught but motivated to fulfill your needs.</p>
+          </div>
+          <div className=" dark:text-white text-5xl flex justify-center gap-16 py-3 text-gray-800">
+            <a href="https://www.instagram.com/ktor.vi/"> <AiFillInstagram /> </a>
+            <a href="https://www.linkedin.com/in/victor-philippe-b54209257/"><AiFillLinkedin /></a>
+            <a href="mailto:me@ktorvi.me" ><AiFillMail /></a>
+          </div>
+          <div className="mx-auto my-10 relative rounded-r-full">
+            <Image className="mx-auto rounded-full w-60 h-60 md:h-96 md:w-96" src={avatar} alt="Victor Philippe standing" />
+          </div>
+        </section>
+        <section className="py-5">
+          <div>
+            <h3 className="dark:text-white  text-3xl py-1">Projects I've worked on</h3>
+            <p className="dark:text-white  text-md py-2 text-gray-800">As a self-taught developer I have built several projects to showcase my competences,
+              in various languages and fulfillng different objetives.</p>
+          </div>
+          <div className="my-3 lg:flex gap-10">
+            <div className="flex-1 justify-between text-center shadow-lg p-10 rounded-corners">
+              <h3 className=" dark:text-white text-lg font-medium pt-8 pb-2 mb-3 ">Blinest Blindtest Bot</h3>
+              <Image className="my-2 mb-7" src={python} alt="python" />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <a href="https://github.com/ktor-vi/bbb" className="dark:text-white py-2 underline">An piloted browser bot to be the best at blindtests on Blinest.com</a>
+              <h4 className="dark:text-white font-bold my-2">Made using :</h4>
+              <ul className="dark:text-white ">
+                <li>Python</li>
+                <li>Selenium</li>
+              </ul>
+            </div>
+            <div className="dark:text-white flex-1 text-center shadow-lg p-10 rounded-corners">
+              <h3 className="dark:text-white text-lg font-medium pt-8 pb-2 mb-3 ">Back My Books</h3>
+              <Image className="my-2 rounded-corners" src={pg} alt="pg" />
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+              <a href="https://github.com/LVannebenne/back-my-books" className="dark:text-white py-2 underline">A project I worked on at the becode bootcamp  on Library management</a>
+              <h4 className="font-bold my-2 dark:text-white ">Made Using :</h4>
+              <ul className="dark:text-white ">
+                <li>NodeJS</li>
+                <li>PostgreSQL</li>
+                <li>GraphQL</li>
+              </ul>
+            </div>
+            <div className="flex-1 text-center shadow-lg p-10 rounded-corners">
+              <h3 className="text-lg font-medium pt-8 pb-2 mb-3 dark:text-white  ">Germaine Collard</h3>
+              <Image className="my-2 mb-3" src={gc} alt="gc" />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <a href="https://germainecollard.be" className="dark:text-white py-2 underline">During my internship at Mwesto Labs, Liège I worked on this shoe shop website  Mainly on the login and sign in parts</a>
+              <h4 className="dark:text-white font-bold my-2">I used :</h4>
+              <ul className="dark:text-white ">
+                <li>Vue</li>
+                <li>Bootstrap</li>
+                <li>NuxtJS</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white ">Educational background</h3>
+            <p className="text-md py-2 text-gray-800 dark:text-white ">I am mainly self taught, but I had the chances to attend various trainings and educational opportunities.</p>
+          </div>
+          <div className="mt-3 lg:flex gap-10">
+            <div className="flex-1 text-center shadow-lg p-10 rounded-corners">
+              <h3 className="text-lg font-medium pt-8 pb-2 mb-3 dark:text-white ">CS 50 - C course from Harvard</h3>
+              <Image className="my-2 mb-20" src={cs50} alt="cs50" />
+              <a href="https://github.com/ktor-vi/bbb" className="dark:text-white y-2 underline">I attended the CS50 Computer Science Course from Harvard in 2023  completed the C programming assignments</a>
+              <h4 className="font-bold my-2 dark:text-white ">I learned :</h4>
+              <ul className="dark:text-white ">
+                <li>C programming basics</li>
+                <li>Pointers and File Management</li>
+                <li>Structs and data structures</li>
+              </ul>
+            </div>
+            <div className="flex-1 text-center shadow-lg p-10 rounded-corners">
+              <h3 className="text-lg font-medium pt-8 pb-2 mb-3 dark:text-white ">Becode Web Bootcamp</h3>
+              <Image className=" h-45 w-45" src={becode} alt="becode" />
+              <a href="https://github.com/ktor-vi/bbb" className="dark:text-white py-2 underline">A Web developer bootcamp in Liège, Belgium I attended in 2019</a>
+              <h4 className="font-bold my-2 dark:text-white  ">I learned :</h4>
+              <ul className="dark:text-white ">
+                <li>NodeJS</li>
+                <li>HTML + CSS</li>
+                <li>Sass</li>
+                <li>ReactJS</li>
+                <li>VueJS</li>
+                <li>PostgreSQL</li>
+                <li>GraphQL</li>
+              </ul>
+            </div>
+            <div className="flex-1 text-center shadow-lg p-10 rounded-corners">
+              <h3 className="text-lg font-medium pt-8 pb-2 mb-3 dark:text-white ">Cisco CCNA Training</h3>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+              <Image className="w-30 my-2 mb-7" src={cisco} alt="cisco" />
+              <a href="https://cisco.com" className=" dark:text-white py-2 underline">I attended a training on Cisco Networking in 2019</a>
+              <h4 className="font-bold my-2 dark:text-white " >I  learned :</h4>
+              <ul className="dark:text-white  ">
+                <li>Basic Networking</li>
+                <li>Management of enterpise networks</li>
+              </ul>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          </div>
+        </section>
+      </main>
+    </div >
+  );
 }
